c0e07614661e3902db38f65122bab1f95c59a4da
Sergej Timofeev <sergej.by@gmail.com>
2025-03-23 12:11:32 +0300

    SPT 3.11 SUPPORT

--- Plugin.cs
+++ Plugin.cs
@@ -7,7 +7,7 @@ using EFT;
 
 namespace armorMod
 {
-    [BepInPlugin("com.dvize.ASS", "dvize.ASS", "1.7.1")]
+    [BepInPlugin("com.dvize.ASS", "dvize.ASS", "1.7.2")]
     public class AssPlugin : BaseUnityPlugin
     {
         internal static ConfigEntry<Boolean> ArmorServiceMode
